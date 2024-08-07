c5b98bf9ce901299aab212be8f6e4b8151d5fd0f
Vimal Vs <60288021+vimal-vs@users.noreply.github.com>
2024-08-07 16:14:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "google_storage_bucket" "my-bucket" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+#
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "tf-jenkins-pipeline-test"
+  project                  = "vimalvs"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
